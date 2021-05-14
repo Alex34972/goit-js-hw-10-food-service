00e1a78dcf6fce703b38685b949e0a2a7dd415a9
Alex34972 <alex_hifi@ukr.net>
2021-05-14 15:49:01 +0300

    Create foodcards.js

---
+++ src/js/foodcards.js
@@ -0,0 +1,6 @@
+import foodCardTempl from '../templates/food-card.hbs';
+console.log(foodCardTempl)
+import '../styles.css';
+import '../menu.json';
+const foodCardContainer = document.querySelector(`.menu`);
+console.log (foodCardContainer)

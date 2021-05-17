9966def8ff1b8a00067b957654acec4bde0afcbb
Alex34972 <alex_hifi@ukr.net>
2021-05-17 18:11:13 +0300

    Update themabody.js

--- src/js/themabody.js
+++ src/js/themabody.js
@@ -1,11 +1,31 @@
-
-const themeControl =document.querySelector(`.theme-switch__toggle`);
-themeControl.addEventListener(`change`,onSwitchTheme);
-function onSwitchTheme(){
-    if (!themeControl.checked) {
-        document.body.classList.add(`light-theme`)
-        document.body.classList.remove(`dark-theme`);         
-    }else{
-        document.body.classList.add(`dark-theme`);
-        document.body.classList.remove(`light-theme`)}
+const themeControl = document.querySelector(`.theme-switch__toggle`);
+const Theme = {
+  LIGHT: "light-theme",
+  DARK: "dark-theme",
 };
+const darkTheme = () => {
+  document.body.classList.add(`${Theme.DARK}`);
+  document.body.classList.remove(`${Theme.LIGHT}`);
+};
+const lightTheme = () => {
+  document.body.classList.add(`${Theme.LIGHT}`);
+  document.body.classList.remove(`${Theme.DARK}`);
+};
+themeControl.addEventListener(`change`, onSwitchTheme);
+function onSwitchTheme() {
+  if (!themeControl.checked) {
+    lightTheme();
+    localStorage.setItem("Theme", Theme.LIGHT);
+    return;
+  }
+  darkTheme();
+  localStorage.setItem("Theme", Theme.DARK);
+}
+const localStorageValue = localStorage.getItem("Theme");
+
+if (localStorageValue === `${Theme.LIGHT}`) {
+  lightTheme();
+} else {
+  darkTheme();
+  themeControl.checked = true;
+}
